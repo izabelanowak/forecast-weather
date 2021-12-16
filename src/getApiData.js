e3a267d7435232f725ab6f2858d039aee6cb856e
Izabela Nowak <nowak.izabela.ni@gmail.com>
2021-12-16 21:02:04 +0100

    initial commit

---
+++ src/getApiData.js
@@ -0,0 +1,7 @@
+import axios from "axios";
+
+export const getApiData = (link) => {
+  return axios
+    .get(link)
+    .then((response) => response.data)
+};
---
+++ src/theme.js
@@ -0,0 +1,8 @@
+export const theme = {
+    breakpoints: {
+        mobile: "577",
+        tablet: "767",
+        tabletLandscape: "991",
+        desktopSmall: "1368",
+    },
+};
