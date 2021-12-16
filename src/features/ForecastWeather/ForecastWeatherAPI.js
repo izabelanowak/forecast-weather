9b0a486636cee32f066c48df0691fd266be88718
Izabela Nowak <nowak.izabela.ni@gmail.com>
2021-12-16 23:12:46 +0100

    add getWeather data from API

---
+++ src/features/ForecastWeather/ForecastWeatherAPI.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+const APIBaseURL = "https://api.openweathermap.org/data/2.5/forecast?";
+const APIKey = "d4e6ee56be175a26bd9e6c27abf0e95c";
+
+export const getWeather = city =>
+  axios.get(`${APIBaseURL}q=${city}&appid=${APIKey}`)
+    .then((response) => response.data);
