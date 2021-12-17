c73f107059123ff8a2945712ce2c6f532fc68af4
Izabela Nowak <nowak.izabela.ni@gmail.com>
2021-12-17 17:35:12 +0100

    add theme to saga and store

--- src/core/rootSaga.js
+++ src/core/rootSaga.js
@@ -1,8 +1,10 @@
 import { all } from "redux-saga/effects";
+import { themeSaga } from "../common/themeSaga";
 import { forecastWeatherSaga } from "../features/forecastWeather/ForecastWeatherSaga";
 
 export default function* rootSaga() {
   yield all([
     forecastWeatherSaga(),
+    themeSaga(),
   ]);
 }
--- src/core/store.js
+++ src/core/store.js
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import createSagaMiddleware from "redux-saga";
 import rootSaga from "./rootSaga";
 import forecastWeatherReducer from "../features/forecastWeather/ForecastWeatherSlice";
+import themeReducer from "../common/themeSlice";
 
 const sagaMiddleware = createSagaMiddleware();
 
 const store = configureStore({
   reducer: {
     forecastWeather: forecastWeatherReducer,
+    theme: themeReducer,
   },
   middleware: [sagaMiddleware],
 });
