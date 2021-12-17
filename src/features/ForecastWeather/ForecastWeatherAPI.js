bf46a64a7de4fdd0fd50cf344e5eaebc89bdb7fa
Izabela Nowak <nowak.izabela.ni@gmail.com>
2021-12-17 23:41:14 +0100

    add metric units

--- src/features/ForecastWeather/ForecastWeatherAPI.js
+++ src/features/ForecastWeather/ForecastWeatherAPI.js
@@ -4,5 +4,5 @@ const APIBaseURL = "https://api.openweathermap.org/data/2.5/forecast?";
 const APIKey = "d4e6ee56be175a26bd9e6c27abf0e95c";
 
 export const getWeather = city =>
-  axios.get(`${APIBaseURL}q=${city}&appid=${APIKey}`)
+  axios.get(`${APIBaseURL}q=${city}&units=metric&appid=${APIKey}`)
     .then((response) => response.data);
