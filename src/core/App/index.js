20d439a1e44a0224ad79217763a19967fc173d6a
Izabela Nowak <nowak.izabela.ni@gmail.com>
2021-12-17 17:36:45 +0100

    add theme and ForecastWeather to App

--- src/core/App/index.js
+++ src/core/App/index.js
@@ -1,15 +1,19 @@
+import { useSelector } from "react-redux";
 import { ThemeProvider } from "styled-components";
 import { Normalize } from "styled-normalize";
+import { selectIsDarkTheme } from "../../common/themeSlice";
 import ForecastWeather from "../../features/forecastWeather/ForecastWeather";
 import { GlobalStyle } from "./GlobalStyle";
-import { theme } from "./theme";
+import { themeDark, themeLight } from "./theme";
 
 function App() {
+  const isDarkTheme = useSelector(selectIsDarkTheme);
+
   return (
-    <ThemeProvider theme={theme}>
+    <ThemeProvider theme={isDarkTheme ? themeDark : themeLight}>
       <Normalize />
       <GlobalStyle />
-      <ForecastWeather/>
+      <ForecastWeather />
     </ThemeProvider>
   );
 }
