b714e7ec916a22d9462c48e0c1a1811be1f25a1f
Izabela Nowak <nowak.izabela.ni@gmail.com>
2021-12-19 00:39:11 +0100

    add key prop to components

--- src/features/ForecastWeather/ForecastWeather/FutureWeather/index.js
+++ src/features/ForecastWeather/ForecastWeather/FutureWeather/index.js
@@ -13,16 +13,19 @@ export const FutureWeather = () => {
       {weather.list.map(weather =>
         <Wrapper>
           <WeatherTile
+            key={weather.dt}
             timestamp={weather.dt * 1000}
             iconCode={weather.weather[0].icon}
             temperature={weather.main.temp}
             description={`${weather.weather[0].main} - ${weather.weather[0].description}`}
           />
           <DataTile
+            key={weather.dt + 1}
             type="pressure"
             data={`${weather.main.pressure} hPa`}
           />
           <DataTile
+            key={weather.dt + 2}
             type="wind"
             data={`${weather.wind.speed} km/h`}
           />
