bc4e507498c1c952246fe661ace8b74522dcaee2
Izabela Nowak <nowak.izabela.ni@gmail.com>
2021-12-18 14:29:29 +0100

    add StatusChecker and CityWeather

--- src/features/ForecastWeather/ForecastWeather/index.js
+++ src/features/ForecastWeather/ForecastWeather/index.js
@@ -1,23 +1,24 @@
+import { useSelector } from "react-redux";
 import ThemeSwitcher from "../../../common/ThemeSwitcher";
+import { selectError, selectLoading } from "../ForecastWeatherSlice";
+import { CityWeather } from "./CityWeather";
 import { Header } from "./Header";
 import { MainContainer } from "./MainContainer";
 import { Search } from "./Search";
-import { TileWrapper } from "./styled";
-import { Tile } from "./Tile";
+import { StatusChecker } from "./StatusChecker";
 
 const ForecastWeather = () => {
+  const isLoading = useSelector(selectLoading);
+  const isError = useSelector(selectError);
+
   return (
     <MainContainer>
       <ThemeSwitcher />
       <Header />
       <Search />
-      <TileWrapper>
-        <Tile />
-        <Tile />
-        <Tile />
-        <Tile />
-        <Tile />
-      </TileWrapper>
+      <StatusChecker isError={isError} isLoading={isLoading}>
+        <CityWeather />
+      </StatusChecker>
     </MainContainer>
   );
 };
