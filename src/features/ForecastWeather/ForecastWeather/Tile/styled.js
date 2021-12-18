fbeef28044569d643f8621a454e9e9981e3ece84
Izabela Nowak <nowak.izabela.ni@gmail.com>
2021-12-19 00:52:43 +0100

    add justify-content center to Wrapper

--- src/features/ForecastWeather/ForecastWeather/Tile/styled.js
+++ src/features/ForecastWeather/ForecastWeather/Tile/styled.js
@@ -7,6 +7,7 @@ export const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  justify-content: center;
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
   margin: 0 10px;
   opacity: 0.8;
