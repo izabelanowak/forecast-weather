93b0aa8e7bd5dd14db709fb3c98006e849925672
Izabela Nowak <nowak.izabela.ni@gmail.com>
2021-12-19 00:52:23 +0100

    add media queries

--- src/features/ForecastWeather/ForecastWeather/CityWeather/styled.js
+++ src/features/ForecastWeather/ForecastWeather/CityWeather/styled.js
@@ -22,11 +22,15 @@ export const CityName = styled.span`
 export const DataTileWrapper = styled.div`
   display: grid;
   grid-gap: 10px;
-  grid-template-columns: 300px 300px 300px;
-  grid-template-rows: 150px 150px;
+  grid-template-columns: repeat(3,1fr);
+  grid-template-rows: repeat(2,1fr);
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.desktopSmall}px) {
+    grid-template-columns: repeat(2,1fr);
+    grid-gap: 4px;
+  }
 
   @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
-    margin-top: 10px;
     grid-template-columns: 1fr 1fr;
     grid-template-rows: 1fr;
     grid-gap: 2px;
