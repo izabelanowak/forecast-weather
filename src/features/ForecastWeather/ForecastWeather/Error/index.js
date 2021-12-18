fe72fff66842ac7c23cc53d15558f9a7de003fc2
Izabela Nowak <nowak.izabela.ni@gmail.com>
2021-12-18 14:28:35 +0100

    change in error message

--- src/features/ForecastWeather/ForecastWeather/Error/index.js
+++ src/features/ForecastWeather/ForecastWeather/Error/index.js
@@ -6,7 +6,7 @@ export const Error = () => {
       <ErrorIcon />
       <Title>Ooops! Something went wrong… </Title>
       <Info>
-        Sorry, failed to load Forecast Weather. Try again later.
+        Sorry, failed to load Forecast Weather. There are no results for your search, please try entering the city name again or try again later.
       </Info>
     </Wrapper>
   );
