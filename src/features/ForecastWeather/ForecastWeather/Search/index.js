e0ba309a7700fac74f44a0bb5775b5e313a2a5f1
Izabela Nowak <nowak.izabela.ni@gmail.com>
2021-12-18 14:15:39 +0100

    add search by city name feature

--- src/features/ForecastWeather/ForecastWeather/Search/index.js
+++ src/features/ForecastWeather/ForecastWeather/Search/index.js
@@ -1,9 +1,37 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchWeather, selectCity, selectQuery, setCity, setQuery } from "../../ForecastWeatherSlice";
 import { Button, Icon, Input, Wrapper } from "./styled";
 
 export const Search = () => {
+  const query = useSelector(selectQuery);
+  const dispatch = useDispatch();
+  const city = useSelector(selectCity);
+
+  const onChange = (value) => {
+    dispatch(setQuery(value));
+  };
+
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+
+    dispatch(setCity(query));
+    dispatch(setQuery(""));
+  };
+
+  useEffect(() => {
+    if (city) {
+      dispatch(fetchWeather(city));
+    }
+  }, [dispatch, city]);
+
   return (
-    <Wrapper>
-      <Input placeholder="Type your city…"></Input>
+    <Wrapper onSubmit={onFormSubmit}>
+      <Input
+        placeholder="Type your city…"
+        value={query}
+        onChange={({ target }) => onChange(target.value)}
+      />
       <Button><Icon />Search</Button>
     </Wrapper>
   );
--- src/features/ForecastWeather/ForecastWeatherSlice.js
+++ src/features/ForecastWeather/ForecastWeatherSlice.js
@@ -6,6 +6,8 @@ const forecastWeatherSlice = createSlice({
     weather: [],
     loading: true,
     error: false,
+    query: "",
+    city: "",
   },
   reducers: {
     fetchWeather: (state) => {
@@ -15,10 +17,17 @@ const forecastWeatherSlice = createSlice({
       state.weather = weather;
       state.loading = false;
       state.error = false;
+      state.query = "";
     },
     fetchWeatherError: (state) => {
       state.loading = false;
       state.error = true;
+    },
+    setQuery: (state, { payload: query }) => {
+      state.query = query;
+    },
+    setCity: (state, { payload: query }) => {
+      state.city = query;
     },
   },
 });
@@ -27,6 +36,8 @@ export const {
   fetchWeather,
   fetchWeatherSuccess,
   fetchWeatherError,
+  setQuery,
+  setCity,
 } = forecastWeatherSlice.actions;
 
 const selectForecastWeatherState = state => state.forecastWeather;
@@ -34,5 +45,7 @@ const selectForecastWeatherState = state => state.forecastWeather;
 export const selectWeather = state => selectForecastWeatherState(state).weather;
 export const selectLoading = state => selectForecastWeatherState(state).loading;
 export const selectError = state => selectForecastWeatherState(state).error;
+export const selectQuery = (state) => selectForecastWeatherState(state).query;
+export const selectCity = (state) => selectForecastWeatherState(state).city;
 
 export default forecastWeatherSlice.reducer;
