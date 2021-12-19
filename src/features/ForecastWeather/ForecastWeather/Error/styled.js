56831bb596771c320cec4f50f26b323904c715a2
Izabela Nowak <nowak.izabela.ni@gmail.com>
2021-12-19 01:23:45 +0100

    chnage color

--- src/features/ForecastWeather/ForecastWeather/Error/styled.js
+++ src/features/ForecastWeather/ForecastWeather/Error/styled.js
@@ -15,14 +15,14 @@ export const Wrapper = styled.div`
 `;
 
 export const ErrorIcon = styled(Error)`
-  stroke: ${({ theme }) => theme.colors.textPrimary};
+  stroke: ${({ theme }) => theme.colors.titleText};
 `;
 
 export const Title = styled.h2`
   font-weight: bold;
   font-size: 24px;
   line-height: 1.2;
-  color: ${({ theme }) => theme.colors.textPrimary};
+  color: ${({ theme }) => theme.colors.titleText};
   
   @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
     font-size: 16px;
@@ -32,7 +32,7 @@ export const Title = styled.h2`
 export const Info = styled.p`
   font-size: 20px;
   line-height: 1.4;
-  color: ${({ theme }) => theme.colors.textPrimary};
+  color: ${({ theme }) => theme.colors.titleText};
   text-align: center;
 
   @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
