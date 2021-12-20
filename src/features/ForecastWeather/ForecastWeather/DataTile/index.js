6fbf45b3b5e60d7cb07a32affc1b668efc546666
Izabela Nowak <nowak.izabela.ni@gmail.com>
2021-12-20 01:03:56 +0100

    add subtitle props

--- src/features/ForecastWeather/ForecastWeather/DataTile/index.js
+++ src/features/ForecastWeather/ForecastWeather/DataTile/index.js
@@ -1,10 +1,15 @@
 import { Data, Title, Wrapper } from "./styled";
 import { switchTileType } from "./switchTileType";
 
-export const DataTile = ({ type, data }) => {
+export const DataTile = ({ type, data, subtitle }) => {
   return (
     <Wrapper>
-      <Title>{switchTileType(type).title}</Title>
+      {subtitle
+        ?
+        <Title>{subtitle}</Title>
+        :
+        <Title>{switchTileType(type).title}</Title>
+      }
       {switchTileType(type).icon}
       <Data>{data}</Data>
     </Wrapper>
