7118eb79965d96d0c959712ea7283faa02f75bce
Izabela Nowak <nowak.izabela.ni@gmail.com>
2021-12-21 23:54:07 +0100

    restructure component

--- src/features/ForecastWeather/ForecastWeather/WeatherTile/index.js
+++ src/features/ForecastWeather/ForecastWeather/WeatherTile/index.js
@@ -16,43 +16,61 @@ import {
 } from "./styled";
 import { ShowMoreButton } from "./ShowMoreButton";
 
-export const WeatherTile = ({ timestamp, iconCode, temperature, minTemperature, maxTemperature, description, mini, future, dayNumber }) => {
-  const date = new Date(timestamp);
+export const WeatherTile =
+  ({
+    timestamp,
+    iconCode,
+    temperature,
+    minTemperature,
+    maxTemperature,
+    description,
+    mini,
+    future,
+    dayNumber,
+    day
+  }) => {
+    const date = new Date(timestamp);
 
-  return (
-    mini ?
-      <HorizontalWrapper>
-        <Time>{getTime(date)}</Time>
-        {switchIcon(iconCode)}
-        <Temperature>{Math.round(temperature)}<TemperatureUnit /></Temperature>
-        <Description>{description}</Description>
-      </HorizontalWrapper>
-      :
-      future ?
-        <>
-          <WeatherWrapper>
-            <TimeWrapper>
-              <Weekday>{getWeekday(date)}</Weekday>
-              <StyledDate>{getDate(date)}</StyledDate>
-            </TimeWrapper>
-            {switchIcon(iconCode)}
-            <Temperature  mini="true">
-              <Label>Min</Label>
-              {Math.round(minTemperature)}<TemperatureUnit  mini="true"/>
-            </Temperature>
-            <Temperature  mini="true">
-              <Label>Max</Label>
-              {Math.round(maxTemperature)}<TemperatureUnit  mini="true"/>
-            </Temperature>
-            <ShowMoreButton dayNumber={dayNumber} />
-          </WeatherWrapper>
-        </>
-        :
-        <Wrapper>
-          <DateWrapper>{formatDate(date)}</DateWrapper>
+    return (
+      mini ?
+        <HorizontalWrapper>
+          <Time>{getTime(date)}</Time>
           {switchIcon(iconCode)}
           <Temperature>{Math.round(temperature)}<TemperatureUnit /></Temperature>
           <Description>{description}</Description>
-        </Wrapper>
-  );
-};
+        </HorizontalWrapper>
+        :
+        day ?
+          <TimeWrapper>
+            <Weekday>{getWeekday(date)}</Weekday>
+            <StyledDate>{getDate(date)}</StyledDate>
+          </TimeWrapper>
+          :
+          future ?
+            <>
+              <WeatherWrapper>
+                <TimeWrapper>
+                  <Weekday>{getWeekday(date)}</Weekday>
+                  <StyledDate>{getDate(date)}</StyledDate>
+                </TimeWrapper>
+                {switchIcon(iconCode)}
+                <Temperature mini="true">
+                  <Label>Min</Label>
+                  {Math.round(minTemperature)}<TemperatureUnit mini="true" />
+                </Temperature>
+                <Temperature mini="true">
+                  <Label>Max</Label>
+                  {Math.round(maxTemperature)}<TemperatureUnit mini="true" />
+                </Temperature>
+                <ShowMoreButton dayNumber={dayNumber} />
+              </WeatherWrapper>
+            </>
+            :
+            <Wrapper>
+              <DateWrapper>{formatDate(date)}</DateWrapper>
+              {switchIcon(iconCode)}
+              <Temperature>{Math.round(temperature)}<TemperatureUnit /></Temperature>
+              <Description>{description}</Description>
+            </Wrapper>
+    );
+  };
