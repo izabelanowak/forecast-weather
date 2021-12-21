8e4b32be089ef3d09dab0c24430b6beeeaf4f1c7
Izabela Nowak <nowak.izabela.ni@gmail.com>
2021-12-21 20:22:01 +0100

    delete opacity

--- src/features/ForecastWeather/ForecastWeather/DataTile/styled.js
+++ src/features/ForecastWeather/ForecastWeather/DataTile/styled.js
@@ -10,7 +10,6 @@ export const Wrapper = styled.div`
   justify-content: space-between;
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
   margin: 10px;
-  opacity: 0.8;
   font-weight: bold;
 
   &:hover {
