7642ffdf07708b9ec7bc9f740e2ede34aeeda77c
Izabela Nowak <nowak.izabela.ni@gmail.com>
2021-12-21 01:51:52 +0100

    add new custom Hook to select weather ona given day

---
+++ src/features/ForecastWeather/ForecastWeather/FutureWeather/useWeatherDay.js
@@ -0,0 +1,31 @@
+import { useSelector } from "react-redux";
+import { selectWeather } from "../../ForecastWeatherSlice";
+
+const isDayNumber = (timestamp, dayNumber) => {
+  const currentDate = new Date();
+  const today = currentDate.getDate();
+  const date = new Date(timestamp * 1000);
+  const day = date.getDate();
+  return day === today + dayNumber;
+};
+
+const createDay = (table, dayNumber) => {
+  const day = [];
+  for (const element of table) {
+    if (isDayNumber(element.dt, dayNumber)) {
+      day.push({
+        dt: element.dt,
+        icon: element.weather[0].icon,
+        temp: element.main.temp,
+        description: element.weather[0].main,
+      });
+    }
+  }
+  return day;
+};
+
+export const useWatherDay = (dayNumber) => {
+  const weather = useSelector(selectWeather);
+
+  return createDay(weather.list, dayNumber);
+};
